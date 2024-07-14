7a10db4ae30a7db2e6c1c771660efb5cb1f12282
Mickle <putilovms@gmail.com>
2024-07-14 16:33:47 +0400

    project init

--- Makefile
+++ Makefile
@@ -2,8 +2,10 @@ PORT ?= 8000
 
 install:
 	poetry install
+
 dev:
-	poetry run flask --app page_analyzer:app run
+	poetry run flask --app page_analyzer:app --debug run
+
 lint:
 	poetry run flake8 page_analyzer
 
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,12 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
-app.secret_key = "secret_key"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
 def home_page():
     return 'Welcome to Flask!'
-
-
-# if __name__ == '__main__':
-#     app.run()
