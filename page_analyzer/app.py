fb4717a3bb41c3f8a12a8f1c909da5c3169e4540
Mickle <putilovms@gmail.com>
2024-07-18 12:21:34 +0400

    add logging

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 import os
 from dotenv import load_dotenv
+import logging
+
+custom_time_format = '%Y-%m-%d %H:%M:%S'
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="page_analyzer.log",
+    filemode="w",
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt=custom_time_format
+)
 
 load_dotenv()
 app = Flask(__name__)
@@ -9,4 +19,6 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.route('/')
 def home_page():
+    url = url_for('home_page')
+    logging.debug(f"Страница загружена: {url}")
     return render_template('index.html')
