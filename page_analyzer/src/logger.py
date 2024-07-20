b5767f78117d51076ba947d4f2f2c623ec487b96
Mickle <putilovms@gmail.com>
2024-07-20 20:50:20 +0400

    modified logger.py

--- page_analyzer/src/logger.py
+++ page_analyzer/src/logger.py
@@ -1,14 +1,44 @@
+import logging.config
 import logging
 
-# custom_time_format = '%Y-%m-%d %H:%M:%S'
-custom_time_format = '%H:%M'
-logging.basicConfig(
-    level=logging.DEBUG,
-    # filename="page_analyzer.log",
-    # filemode="w",
-    format="%(asctime)s %(name)s %(levelname)s %(message)s",
-    datefmt=custom_time_format
-)
+# CUSTOM_TIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+CUSTOM_TIME_FORMAT = '%H:%M'
+LOG_FILENAME = "page_analyzer.log"
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "formatters": {
+        "default": {
+            "format": "%(asctime)s %(name)s %(levelname)s %(message)s",
+            "datefmt": CUSTOM_TIME_FORMAT,
+        },
+    },
+    "handlers": {
+        "logfile": {
+            "formatter": "default",
+            "level": "DEBUG",
+            "filename": LOG_FILENAME,
+            "class": "logging.FileHandler",
+            "mode": "w",
+        },
+        "stdout": {
+            "formatter": "default",
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+        }
+    },
+    "loggers": {},
+    "root": {
+        "level": "DEBUG",
+        "handlers": [
+            "logfile",
+            "stdout"
+        ],
+    },
+}
+
+logging.config.dictConfig(LOGGING_CONFIG)
 
 log = logging.getLogger(__name__)
 log.info('Logger is active!')
