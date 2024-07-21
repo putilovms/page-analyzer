de7490a7b8ef8775409f13455b0487da612afba8
Mickle <putilovms@gmail.com>
2024-07-21 13:41:34 +0400

    refacroting website.py

--- page_analyzer/src/website.py
+++ page_analyzer/src/website.py
@@ -3,6 +3,7 @@ import urllib.parse
 from typing import Optional, Any
 from requests import Response
 from bs4 import BeautifulSoup
+from . import from_db
 
 log = logging.getLogger(__name__)
 
@@ -15,36 +16,36 @@ def normalize(site_name: str) -> str:
     return new_url
 
 
-def get_site(id: int, source: Any) -> Any:
-    site = source.get_site(id)
+def get_site(id: int) -> Any:
+    site = from_db.get_site(id)
     log.debug(f'Данные сайта - {site}')
     return site
 
 
-def get_id_site(site_name: str, source: Any) -> Optional[int]:
+def get_id_site(site_name: str) -> Optional[int]:
     site_name = normalize(site_name)
-    id = source.get_id_site(site_name)
+    id = from_db.get_id_site(site_name)
     log.debug(f'ID = {id}')
     return id
 
 
-def add_site(site_name: str, source: Any) -> int:
+def add_site(site_name: str) -> int:
     site_name = normalize(site_name)
-    id = source.add_site(site_name)
+    id = from_db.add_site(site_name)
     log.debug(f'Сайт добавлен. ID = {id}')
     return id
 
 
-def get_all_sites(source: Any) -> list:
-    sites = source.get_all_sites()
+def get_all_sites() -> list:
+    sites = from_db.get_all_sites()
     log.debug(f'Данные о сайтах получены: {sites}')
     return sites
 
 
-def check_site(id: int, response: Response, source: Any) -> None:
+def check_site(id: int, response: Response) -> None:
     check_data = get_check_data(response)
     log.debug(f'Данные проверки: {check_data}')
-    source.add_check_site(id, check_data)
+    from_db.add_check_site(id, check_data)
     log.debug(f'Добавлена проверка для ID = {id}')
     return None
 
@@ -63,7 +64,7 @@ def get_check_data(response: Response) -> dict:
     return check_data
 
 
-def get_checks(url_id: int, source: Any) -> list:
-    checks = source.get_checks(url_id)
+def get_checks(url_id: int) -> list:
+    checks = from_db.get_checks(url_id)
     log.debug(f'Данные о проверках получены: {checks}')
     return checks
